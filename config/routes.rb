f4e05268853633844b322008be9ed53e841fb5bc
Jonny Gates <jonny.gates@outlook.com>
2019-02-19 11:27:45 +0000

    created tasks model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Read all tasks
+  # get 'tasks', to: 'tasks#index'
+
+  # # Read the details of one restaurant
+  # get 'tasks/:id', to: 'tasks#show'
+
+  # # create a restaurant 2 REQUESTS
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+
+  # # update a restaurant
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # delete a restaurant
+# delete 'tasks/:id', to: 'tasks#destroy'
 end

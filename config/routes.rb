79173cb8ed945e10e832fe8b2392229e5ba25420
Jonny Gates <jonny.gates@outlook.com>
2019-02-19 12:08:06 +0000

    added show, new, create, edit and update actions to tasks

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
 
   # # Read the details of one restaurant
-  # get 'tasks/:id', to: 'tasks#show'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 
   # # create a restaurant 2 REQUESTS
-  # get 'tasks/new', to: 'tasks#new'
-  # post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
 
   # # update a restaurant
-  # get 'tasks/:id/edit', to: 'tasks#edit'
-  # patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
 
   # # delete a restaurant
 # delete 'tasks/:id', to: 'tasks#destroy'
